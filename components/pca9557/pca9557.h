e3cd3375a9fa559dcf2319ab854a1e1f7144a7ac
DIY Baterías LiFePO4 <80992172+diybateriaslifepo4@users.noreply.github.com>
2025-02-16 17:55:57 +0100

    sdsad

--- components/pca9557/pca9557.h
+++ components/pca9557/pca9557.h
@@ -1,23 +1,23 @@
 #include "esphome.h"
-#include "Wire.h"
 #include "esphome/components/output/float_output.h"
+#include "esphome/components/i2c/i2c.h"
 
 namespace pca9557 {
 
-class PCA9557Output : public esphome::Component, public esphome::output::FloatOutput {
+class PCA9557Output : public esphome::i2c::I2CDevice, public esphome::output::FloatOutput {
  public:
-  PCA9557Output(uint8_t address = 0x18) : address_(address) {}
+  PCA9557Output(uint8_t address = 0x18) : esphome::i2c::I2CDevice(address) {}
 
   void setup() override {
     esphome::delay(500); // Espera 500ms para dar tiempo a la inicialización del touchpad
     // Configure all pins as outputs (register 0x03)
-    uint8_t config_data[] = {0x03, 0x00};
-    if (!Wire.writeBytes(address_, config_data, 2)) {
+    uint8_t config_data[2] = {0x03, 0x00};
+    if (!this->write_array(config_data, 2)) {
       ESP_LOGE("PCA9557", "Failed to configure pins as outputs");
     }
     // Set initial state to all off (register 0x01)
-    uint8_t output_data[] = {0x01, 0x00};
-    if (!Wire.writeBytes(address_, output_data, 2)) {
+    uint8_t output_data[2] = {0x01, 0x00};
+    if (!this->write_array(output_data, 2)) {
       ESP_LOGE("PCA9557", "Failed to set initial state");
     }
   }
@@ -25,14 +25,10 @@ class PCA9557Output : public esphome::Component, public esphome::output::FloatOu
   void write_state(float state) override {
     uint8_t current_state;
     // Read current state (register 0x00)
-    Wire.beginTransmission(address_);
-    Wire.write(0x00);
-    Wire.endTransmission();
-    if (Wire.requestFrom(address_, (uint8_t)1) != 1) {
+    if (!this->read_byte(0x00, &current_state)) {
       ESP_LOGE("PCA9557", "Failed to read current state");
       return;
     }
-    current_state = Wire.read();
 
     // Update pin 1 (backlight) while preserving other pins
     if (state > 0.5f) {
@@ -42,8 +38,8 @@ class PCA9557Output : public esphome::Component, public esphome::output::FloatOu
     }
 
     // Write new state (register 0x01)
-    uint8_t output_data[] = {0x01, current_state};
-    if (!Wire.writeBytes(address_, output_data, 2)) {
+    uint8_t output_data[2] = {0x01, current_state};
+    if (!this->write_array(output_data, 2)) {
       ESP_LOGE("PCA9557", "Failed to write new state");
     }
   }
