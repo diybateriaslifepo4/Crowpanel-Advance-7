9298a306709693f4baa999e50cc6c01c5d4f51a2
DIY Baterías LiFePO4 <80992172+diybateriaslifepo4@users.noreply.github.com>
2025-02-16 18:07:35 +0100

    sdsad

--- components/pca9557/pca9557.h
+++ components/pca9557/pca9557.h
@@ -1,3 +1,10 @@
+// Add forward declaration for I2CMaster.
+namespace esphome {
+  namespace i2c {
+    class I2CMaster;
+  }
+}
+
 #include "esphome.h"
 #include "esphome/components/i2c/i2c.h"
 // Removed: #include "esphome/core/hal/i2c.h"  // No such file, base header already provides I2CMaster.
